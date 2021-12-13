e42ff0260f5d91106c90982089a533899907e57a
nappeni <nappeni@daum.net>
2021-12-13 10:59:57 +0900

    20211213

---
+++ class-note/1_type-basic.ts
@@ -0,0 +1,29 @@
+// ts 문자열
+let str:string = 'hello';
+
+// ts 숫자
+let num:number = 10;
+
+// ts 배열
+let arr: Array<number> = [1,2,3];
+let heroes:Array<string> = ['Capt','Thor','Hulk', 10];
+let items:number[] = [1,2,3];
+
+// ts 튜플
+let address:[string, number] = ['gangnam',100];
+
+// ts 객체
+let obj:object = {
+
+}
+//let person:object = {
+    //name:'capt',
+    //age:100
+//}
+let person:{name:string, age:number} = {
+    name: 'thor',
+    age: 100
+}
+
+// ts 진위값
+let show: boolean = true;
